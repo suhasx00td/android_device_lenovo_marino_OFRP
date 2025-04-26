8a53f42578a9f9444b7ef5f4c022eee32b772fa5
suhasx00td <m.suhashkumar@gmail.com>
2025-04-26 13:12:36 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,10 +36,22 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
-	# Version & Variant
-	export FOX_VERSION="R12.1"
-	export FOX_VARIANT="A8"
-	export FOX_BUILD_TYPE="Stable"
+ # Version & Variant
+export FOX_VERSION="R12.1"
+export FOX_VARIANT="A8"
+export FOX_BUILD_TYPE="Stable"
+
+ # OrangeFox Addons
+export FOX_ENABLE_APP_MANAGER=1
+
+ # Binaries & Tools
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+ ##export FOX_REPLACE_BUSYBOX_PS=1
  
 	lunch twrp_$FDEVICE-eng
 	# let's see what are our build VARs
